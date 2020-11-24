9c308e89c4367c94f6c54274c17cfb8c802789b3
Shaun Bathgate <shaun.bathgate@gov.bc.ca>
2020-11-23 22:17:49 -0800

    Update terraform

--- docker/setup/terraform/01_provider.tf
+++ docker/setup/terraform/01_provider.tf
@@ -1,4 +1,12 @@
-variable "client_secret" {
+# Configure Azure storage to manage Terraform State
+# ref: https://docs.microsoft.com/en-us/azure/developer/terraform/store-state-in-azure-storage
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "tstate"
+    storage_account_name  = "tstate09762"
+    container_name        = "tstate"
+    key                   = "terraform.tfstate"
+  }
 }
 
 # Configure the Microsoft Azure Provider
