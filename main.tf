aa672b2819ce6722730ed3de8c32e79fb35f004f
Shaun Bathgate <shaun.bathgate@gov.bc.ca>
2020-11-26 21:54:26 -0800

    Update terraform

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tstate"
-    storage_account_name = "tstate32033"
+    storage_account_name = "tstate20838"
     container_name       = "tstate"
     key                  = "terraform.tfstate"
   }
